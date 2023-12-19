9e6546236b3791d2bfd6d1dc11207fabe86c48a3
Vincent Bourrier <vincent.bourrier@unige.ch>
2023-12-19 00:31:06 +0000

    Update conf.py

--- Docs/source/conf.py
+++ Docs/source/conf.py
@@ -17,7 +17,7 @@ import glob
 #Identify whether doc is generated on gitlab or locally
 conf_path = os.path.abspath(os.path.dirname(__file__))
 if conf_path=='/builds/bourrier/antaress/Docs/source':
-	print(sys.path)
+	sys.path.insert(0, os.path.abspath('../../Method/'))
 	sys.path.insert(0, os.path.abspath('/builds/bourrier/antaress/Method/'))
 	for antaress_dir in glob.glob('/builds/bourrier/antaress/Method/ANTARESS_*/'):
 		sys.path.insert(0, os.path.abspath(antaress_dir))
