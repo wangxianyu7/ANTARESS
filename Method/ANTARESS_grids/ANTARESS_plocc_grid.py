2024-01-19 - Included the presence of spots in the planet-occulted region. Still not completely sure the way I did it is correct. - Included the possibility for spots to have their own rotation period. Still need to migrate the spot rotation parameter to the appropriate file and add spot alpha and beta rotational parameters. - Updated the GIF function. - Fixed some minor issues.

Method/ANTARESS_grids/ANTARESS_plocc_grid.py
@@ -5,7 +5,7 @@ from utils import stop,closest,np_poly,npint,np_interp,np_where1D,datasave_npz,d
 from constant_data import Rsun,c_light
 import lmfit
 from lmfit import Parameters
-from ANTARESS_grids.ANTARESS_coord import frameconv_LOS_to_InclinedStar,frameconv_InclinedStar_to_LOS,calc_pl_coord
+from ANTARESS_grids.ANTARESS_coord import frameconv_LOS_to_InclinedStar,frameconv_InclinedStar_to_LOS,frameconv_InclinedStar_to_Star,calc_pl_coord
 from ANTARESS_routines.ANTARESS_data_align import align_data
 from ANTARESS_analysis.ANTARESS_inst_resp import convol_prof
 from ANTARESS_grids.ANTARESS_star_grid import calc_CB_RV,get_LD_coeff,calc_st_sky,calc_Isurf_grid,calc_RVrot
@@ -41,7 +41,7 @@ def calc_plocc_prop(system_param,gen_dic,theo_dic,coord_dic,inst,vis,data_dic,ca
 
         if mock_dic!={} and mock_dic['use_spots']:
             params['use_spots']=True
-            for spot_param in mock_dic['spots_prop'][inst][vis].keys():
+            for spot_param in list(mock_dic['spots_prop'][inst][vis].keys()):
                 params[spot_param]=mock_dic['spots_prop'][inst][vis][spot_param]
             #Figuring out the number of spots
             num_spots = 0
@@ -302,6 +302,8 @@ def sub_calc_plocc_prop(key_chrom,args,par_list_gen,transit_pl,system_param,theo
         n_spots = param['num_spots']
         #Initialize the dictionary that will contain spot presence
         cond_spots_all = np.zeros([n_exp,n_spots], dtype=bool)
+        #Retrieve spot rotational velocity
+        param['om_eq_spots']=system_spot_prop['achrom']['veq_spots'][0]/star_params['Rstar_km']
 
         #Looping over all exposures
         for isub_exp, iexp in enumerate(iexp_list):
@@ -509,6 +511,22 @@ def sub_calc_plocc_prop(key_chrom,args,par_list_gen,transit_pl,system_param,theo
             n_osamp_exp_eff_sp = 0
 
             for iosamp in range(n_osamp_exp):
+                #Need to define a reduced version of the spot dictionary in this oversampled position. This is required
+                #if we want to account for presence of spots in planet-occulted regions.
+                reduced_spot_prop_oversamp={}
+                spots_are_visible = False
+                for spot in spots_in_exp:
+                    reduced_spot_prop_oversamp[spot]={}
+                    reduced_spot_prop_oversamp[spot]['atten']=spots_prop_all_exp[iexp][spot]['atten']
+                    reduced_spot_prop_oversamp[spot]['ang_rad']=spots_prop_all_exp[iexp][spot]['ang_rad']
+                    temp_long = np.arcsin(x_oversamp_sp[spot][iosamp] / np.cos(spots_prop_all_exp[iexp][spot]['lat_rad_exp_center']))
+                    reduced_spot_prop_oversamp[spot]['cos_lat_exp_center'] = np.cos(spots_prop_all_exp[iexp][spot]['lat_rad_exp_center'])
+                    reduced_spot_prop_oversamp[spot]['cos_long_exp_center'] = np.cos(temp_long)
+                    reduced_spot_prop_oversamp[spot]['sin_lat_exp_center'] = np.sin(spots_prop_all_exp[iexp][spot]['lat_rad_exp_center'])
+                    reduced_spot_prop_oversamp[spot]['sin_long_exp_center'] = np.sin(temp_long)
+                    spots_are_visible |= is_spot_visible(star_params['istar_rad'], temp_long, spots_prop_all_exp[iexp][spot]['lat_rad_exp_center'], reduced_spot_prop_oversamp[spot]['ang_rad'], star_params['f_GD'], star_params['RpoleReq'])
+
+
                 ## Planets ##
 
                 #Dictionary telling us which planets have been processed in which chromatic mode and band.
@@ -526,11 +544,10 @@ def sub_calc_plocc_prop(key_chrom,args,par_list_gen,transit_pl,system_param,theo
                     y_st_sky_max = y_st_sky_pos+theo_dic['y_st_sky_grid_pl'][pl_loc]
                     
                     #Calculating properties
-                    # cond_occ_pl = False
                     for subkey_chrom in key_chrom:
                         for iband in range(system_prop[subkey_chrom]['nw']):
                             Focc_star_pl[subkey_chrom][iband],cond_occ_pl = calc_occ_region_prop(line_occ_HP[subkey_chrom][iband],cond_occ_pl,iband,args,system_prop[subkey_chrom],iosamp,pl_loc,pl_proc[subkey_chrom][iband],theo_dic['Ssub_Sstar_pl'][pl_loc],x_st_sky_max,y_st_sky_max,system_prop[subkey_chrom]['cond_in_RpRs'][pl_loc][iband],par_list,par_star,theo_dic['Istar_norm_'+subkey_chrom],\
-                                                                                  x_oversamp_pl,y_oversamp_pl,lambda_rad_pl,par_star,sum_prop_dic[subkey_chrom][pl_loc],coord_reg_dic[subkey_chrom][pl_loc],range_dic[subkey_chrom][pl_loc],range_par_list,Focc_star_pl[subkey_chrom][iband],star_params,cb_band_dic[subkey_chrom][iband],theo_dic)
+                                                                                  x_oversamp_pl,y_oversamp_pl,lambda_rad_pl,par_star,sum_prop_dic[subkey_chrom][pl_loc],coord_reg_dic[subkey_chrom][pl_loc],range_dic[subkey_chrom][pl_loc],range_par_list,Focc_star_pl[subkey_chrom][iband],star_params,cb_band_dic[subkey_chrom][iband],theo_dic, spot_occ=spots_are_visible, reduced_spot_prop=reduced_spot_prop_oversamp)
          
                             #Cumulate line profile from planet-occulted cells
                             #    - in high-precision mode there is a single subkey_chrom and achromatic band, but several planets may have been processed
@@ -578,7 +595,6 @@ def sub_calc_plocc_prop(key_chrom,args,par_list_gen,transit_pl,system_param,theo
                     spot_prop_oversamp['sin_lat_exp_center'] = np.sin(spot_prop_oversamp['lat_rad_exp_center'])
                     spot_prop_oversamp['is_visible'] = is_spot_visible(star_params['istar_rad'], spot_prop_oversamp['long_rad_exp_center'], spot_prop_oversamp['lat_rad_exp_center'], spot_prop_oversamp['ang_rad'], star_params['f_GD'], star_params['RpoleReq'])
 
-                    # cond_occ_sp = False
 
                     #Going over the chromatic modes
                     for subkey_chrom in key_chrom:
@@ -741,7 +757,7 @@ def sub_calc_plocc_prop(key_chrom,args,par_list_gen,transit_pl,system_param,theo
 
 
 def calc_occ_region_prop(line_occ_HP_band,cond_occ,iband,args,system_prop,idx,pl_loc,pl_proc_band,Ssub_Sstar,x_st_sky_max,y_st_sky_max,cond_in_RpRs,par_list,param,Istar_norm_band,x_pos_pl,y_pos_pl,lambda_rad_pl,par_star,sum_prop_dic_pl,\
-                         coord_reg_dic_pl,range_reg_pl,range_par_list,Focc_star_band,star_params,cb_band,theo_dic):
+                         coord_reg_dic_pl,range_reg_pl,range_par_list,Focc_star_band,star_params,cb_band,theo_dic,spot_occ = False, reduced_spot_prop={}):
     r"""**Planet-occulted properties: region**
 
     Calculates the average and summed properties from a planet-occulted stellar surface region during an exposure.
@@ -762,6 +778,10 @@ def calc_occ_region_prop(line_occ_HP_band,cond_occ,iband,args,system_prop,idx,pl
     #Identifying occulted stellar cells in the sky-projected and star star rest frame
     n_pl_occ = calc_st_sky(coord_grid,star_params)
     
+    #Making an array that will keep track of which cells are occulted by the spots
+    if spot_occ:
+        for spots in list(reduced_spot_prop.keys()):
+            coord_grid[spots+'_flag_map']=np.zeros(len(coord_grid['x_st_sky']), dtype=bool)
     #Star is effectively occulted
     #    - when the expositions are oversampled, some oversampled positions may put the planet beyond the stellar disk, with no points behind the star
     if n_pl_occ>0:
@@ -784,6 +804,26 @@ def calc_occ_region_prop(line_occ_HP_band,cond_occ,iband,args,system_prop,idx,pl
       
             #Store planet as processed in current band
             pl_proc_band+=[pl_loc]     
+        #--------------------------------
+        #Account for spot occultation in planet-occulted region
+        if spot_occ:
+            #Identify the cells in the planet-occulted region that are spotted
+            for spot in list(reduced_spot_prop.keys()):
+                #Retrieve coordinates of the planet-occulted region in the inclined star frame
+                new_x_sky_grid = coord_grid['x_st_sky']
+                new_y_sky_grid = coord_grid['y_st_sky']
+                new_z_sky_grid = np.sqrt(1 - new_x_sky_grid**2 - new_y_sky_grid**2)
+
+                ##Move coordinates to the (non-inclined) star frame and then the spot reference frame
+                x_st_grid, y_st_grid, z_st_grid = frameconv_InclinedStar_to_Star(new_x_sky_grid, new_y_sky_grid, new_z_sky_grid, star_params['istar_rad'])
+                x_spot_grid = x_st_grid*reduced_spot_prop[spot]['cos_long_exp_center'] - z_st_grid*reduced_spot_prop[spot]['sin_long_exp_center']
+                y_spot_grid = y_st_grid*reduced_spot_prop[spot]['cos_lat_exp_center'] - (z_st_grid*reduced_spot_prop[spot]['cos_long_exp_center'] + x_st_grid*reduced_spot_prop[spot]['sin_long_exp_center']) * reduced_spot_prop[spot]['sin_lat_exp_center']
+                z_spot_grid = y_st_grid*reduced_spot_prop[spot]['sin_lat_exp_center'] + (z_st_grid*reduced_spot_prop[spot]['cos_long_exp_center'] + x_st_grid*reduced_spot_prop[spot]['sin_long_exp_center']) * reduced_spot_prop[spot]['cos_lat_exp_center']
+                cond_in_sp = (x_spot_grid**2. + y_spot_grid**2. <= reduced_spot_prop[spot]['ang_rad']**2)
+
+                #Updating the flag map
+                coord_grid[spot+'_flag_map'] |= cond_in_sp
+
 
         #--------------------------------
 
@@ -791,6 +831,13 @@ def calc_occ_region_prop(line_occ_HP_band,cond_occ,iband,args,system_prop,idx,pl
         coord_grid['nsub_star'] = n_pl_occ
         _,_,mu_grid_star,Fsurf_grid_star,Ftot_star,_ = calc_Isurf_grid([iband],coord_grid['nsub_star'],system_prop,coord_grid,star_params,Ssub_Sstar,Istar_norm = Istar_norm_band,region = 'pl',Ssub_Sstar_ref = theo_dic['Ssub_Sstar'])
         coord_grid['mu'] = mu_grid_star[:,0]
+
+        #Accounting for the spots' emission
+        if spot_occ:
+            for spot in list(reduced_spot_prop.keys()):
+                Fsurf_grid_star[:, iband] = np.where(coord_grid[spot+'_flag_map'] == True, Fsurf_grid_star[:, iband]*(1-reduced_spot_prop[spot]['atten']), Fsurf_grid_star[:, iband])
+                # Fsurf_grid_star[:, iband] = np.where(coord_grid[spot+'_flag_map'] == True, Fsurf_grid_star[:, iband] - (Fsurf_grid_star[:, iband] * (1-reduced_spot_prop[spot]['atten'])), Fsurf_grid_star[:, iband]) <-- other possibility
+            Ftot_star = np.sum(Fsurf_grid_star, axis=0)
 
         #Scale continuum level
         Fsurf_grid_star*=param['cont']

Method/ANTARESS_grids/ANTARESS_spots.py
@@ -118,7 +118,7 @@ def retrieve_spots_prop_from_param(star_params, param, inst, vis, t_bjd):
         
         # Spot longitude
         sin_lat = np.sin(lat_rad)
-        P_spot = 2*np.pi/((1.-param['alpha_rot']*sin_lat**2.-param['beta_rot']*sin_lat**4.)*star_params['om_eq']*3600.*24.)
+        P_spot = 2*np.pi/((1.-param['alpha_rot']*sin_lat**2.-param['beta_rot']*sin_lat**4.)*param['om_eq_spots']*3600.*24.)
         Tcen_sp = spots_prop[spot]['Tcenter'] - 2400000.
         long_rad = (t_bjd-Tcen_sp)/P_spot * 2*np.pi
         
@@ -266,7 +266,7 @@ def new_retrieve_spots_prop_from_param(star_params, param, inst, vis, t_bjd, exp
         
         # Spot longitude - varies over time
         sin_lat = np.sin(lat_rad)
-        P_spot = 2*np.pi/((1.-param['alpha_rot']*sin_lat**2.-param['beta_rot']*sin_lat**4.)*star_params['om_eq']*3600.*24.)
+        P_spot = 2*np.pi/((1.-param['alpha_rot']*sin_lat**2.-param['beta_rot']*sin_lat**4.)*param['om_eq_spots']*3600.*24.)
         Tcen_sp = spots_prop[spot]['Tcenter'] - 2400000.
         long_rad_center = (t_bjd_center-Tcen_sp)/P_spot * 2*np.pi
         long_rad_start = (t_bjd_start-Tcen_sp)/P_spot * 2*np.pi
@@ -361,7 +361,7 @@ def spot_occ_region_grid(RspRs, nsub_Dsp):
     Ssub_Sstar=d_sub*d_sub/np.pi
 
     #Coordinates of points discretizing the enclosing square
-    cen_sub=-RspRs+(np.arange(nsub_Dsp)+0.5)*d_sub            
+    cen_sub=-RspRs+(np.arange(-2, nsub_Dsp+2)+0.5)*d_sub            
     xy_st_sky_grid=np.array(list(it_product(cen_sub,cen_sub)))
 
     # #Keeping only grid points behind the spot
@@ -587,7 +587,7 @@ def new_new_calc_spotted_region_prop(line_occ_HP_band, cond_occ, spot_prop, iban
 
 ########################################################################################################################
 ########################################################################################################################
-#################################################### UNUSED FUNCTIONs  #################################################
+#################################################### UNUSED FUNCTIONS  #################################################
 ########################################################################################################################
 ########################################################################################################################
 

Method/ANTARESS_plots/ANTARESS_plot_settings.py
@@ -7746,6 +7746,7 @@ def ANTARESS_plot_settings(plot_dic,gen_dic,data_dic,glob_fit_dic):
 
         #     plot_settings[key_plot]['t_BJD'] = { 'inst':'ESPRESSO','vis':'mock_vis','t':54560.806755574+np.array([-0.5,-0.2,0.,0.2,0.5])/24. }
         if gen_dic['star_name']=='AUMic':
+            # plot_settings[key_plot]['t_BJD'] = {'inst':'ESPRESSO', 'vis':'mock_vis', 't':  2458330.39051+np.linspace(-0.15,0.15,200)}--plotting purposes
             # plot_settings[key_plot]['t_BJD'] = {'inst':'ESPRESSO', 'vis':'mock_vis', 't': 2458330.39051 + np.linspace(-3.5, 3.5, 5)}
             plot_settings[key_plot]['t_BJD'] = None
             
@@ -7755,7 +7756,7 @@ def ANTARESS_plot_settings(plot_dic,gen_dic,data_dic,glob_fit_dic):
         #    - this is only used if we plot multiple exposures.
         plot_settings[key_plot]['GIF_generation']=True & False
         if gen_dic['star_name']=='AUMic':
-            plot_settings[key_plot]['GIF_generation']=True
+            plot_settings[key_plot]['GIF_generation']=True&False
 
         if gen_dic['star_name']=='V1298tau':
             plot_settings[key_plot]['GIF_generation']=True
@@ -8008,7 +8009,9 @@ def ANTARESS_plot_settings(plot_dic,gen_dic,data_dic,glob_fit_dic):
         # if gen_dic['star_name']=='HD209458':   #ANTARESS I multi-pl           
         #     plot_settings[key_plot]['n_stcell']=41.             
         if gen_dic['star_name']=='WASP76':   #ANTARESS I       
-            plot_settings[key_plot]['n_stcell']=251.         
+            plot_settings[key_plot]['n_stcell']=251.  
+        if gen_dic['star_name']=='AUMic':    
+            plot_settings[key_plot]['n_stcell']=51.       
         
         #Number of cells on a diameter of planets (must be odd)
         #    - leave undefined for default settings to be used       

Method/ANTARESS_plots/ANTARESS_plots_all.py
@@ -25,7 +25,7 @@ from ANTARESS_analysis.ANTARESS_model_prof import gauss_intr_prop,dgauss,cust_mo
 from ANTARESS_corrections.ANTARESS_detrend import detrend_prof_gen
 from ANTARESS_corrections.ANTARESS_interferences import def_wig_tab,calc_chrom_coord,calc_wig_mod_nu_t
 from ANTARESS_grids.ANTARESS_coord import calc_pl_coord_plots,calc_pl_coord,calc_rv_star_HR,frameconv_LOS_to_InclinedStar,frameconv_InclinedStar_to_LOS,get_timeorbit,\
-    calc_zLOS_oblate,frameconv_Star_to_InclinedStar,calc_tr_contacts
+    calc_zLOS_oblate,frameconv_Star_to_InclinedStar,frameconv_InclinedStar_to_Star,calc_tr_contacts
 from ANTARESS_routines.ANTARESS_calib import cal_piecewise_func
 from ANTARESS_grids.ANTARESS_star_grid import get_LD_coeff,calc_CB_RV,calc_RVrot,calc_Isurf_grid,calc_st_sky
 from ANTARESS_grids.ANTARESS_plocc_grid import occ_region_grid,sub_calc_plocc_prop
@@ -4746,7 +4746,7 @@ def ANTARESS_plot_functions(system_param,plot_dic,data_dic,gen_dic,coord_dic,the
         plot_options[key_plot]['GIF_generation'] = False
         
         #FPS for gif
-        plot_options[key_plot]['fps'] = 5
+        plot_options[key_plot]['fps'] = 40
 
         #--------------------------------------
 
@@ -10696,6 +10696,8 @@ def ANTARESS_plot_functions(system_param,plot_dic,data_dic,gen_dic,coord_dic,the
                     params['alpha_rot'] = star_params['alpha_rot']
                     params['beta_rot'] = star_params['beta_rot']       
                  
+                #Retrieve spot rotational velocity
+                params['om_eq_spots']=data_dic['DI']['spots_prop']['achrom']['veq_spots'][0]/star_params['Rstar_km']
 
                 #Calculate the flux of star grid, at all the exposures considered
                 star_flux_before_spot = deepcopy(Fsurf_grid_star[:,iband])
@@ -10715,7 +10717,7 @@ def ANTARESS_plot_functions(system_param,plot_dic,data_dic,gen_dic,coord_dic,the
                             _, spotted_tiles = calc_spotted_tiles(spots_prop[spot], coord_grid['x_st_sky'], coord_grid['y_st_sky'], coord_grid['z_st_sky'], 
                                                                    {}, params, use_grid_dic = False)
                                                                    
-                            star_flux_before_spot[spotted_tiles] *=  spots_prop[spot]['atten'] 
+                            star_flux_before_spot[spotted_tiles] *=  (1-spots_prop[spot]['atten'])
                     
                     if plot_options[key_plot]['spot_overlap']:      
                         Fsurf_grid_star[:,iband] = np.minimum(Fsurf_grid_star[:,iband], star_flux_before_spot)
@@ -10725,7 +10727,7 @@ def ANTARESS_plot_functions(system_param,plot_dic,data_dic,gen_dic,coord_dic,the
                 #If no times provided for the plotting, then generate some
                 else:
                     # Compute BJD of images 
-                    if plot_options[key_plot]['plot_spot_all_Peq'] : t_all_spot = np.linspace(   0  ,   2*np.pi/(star_params['om_eq']*3600.*24.) ,plot_options[key_plot]['n_image_spots'])
+                    if plot_options[key_plot]['plot_spot_all_Peq'] : t_all_spot = np.linspace(   0  ,   2*np.pi/(params['om_eq_spots']*3600.*24.) ,plot_options[key_plot]['n_image_spots'])
                     else:
                         dbjd =  (plot_options[key_plot]['time_range_spot'][1]-plot_options[key_plot]['time_range_spot'][0])/plot_options[key_plot]['n_image_spots']
                         n_in_visit = int((plot_options[key_plot]['time_range_spot'][1]-plot_options[key_plot]['time_range_spot'][0])/dbjd)
@@ -10743,14 +10745,14 @@ def ANTARESS_plot_functions(system_param,plot_dic,data_dic,gen_dic,coord_dic,the
                             if spots_prop[spot]['is_visible']:
                                 _, spotted_tiles = calc_spotted_tiles(spots_prop[spot], coord_grid['x_st_sky'], coord_grid['y_st_sky'], coord_grid['z_st_sky'], 
                                                                         {}, params, use_grid_dic = False)
-                                # if t_exp == t_all_spot[-1]:
+                                # if t_exp == t_all_spot[int(len(t_all_spot)/2)+22]:
                                 star_flux_exp[spotted_tiles] *=  (1-spots_prop[spot]['atten'])
                             
 
                             #Testing how to make 
-                            # if t_exp == t_all_spot[-1]:
+                            # if t_exp == t_all_spot[int(len(t_all_spot)/2)+22]:
                             #     # These coordinates are in the inclined star frame
-                            #     x_grid_test, y_grid_test, _ = spot_occ_region_grid(spots_prop[spot]['ang_rad'], 30)
+                            #     x_grid_test, y_grid_test, _ = spot_occ_region_grid(spots_prop[spot]['ang_rad'], 35)
 
                             #     new_x_grid_test = x_grid_test + spots_prop[spot]['x_sky_exp_center']
                             #     new_y_grid_test = y_grid_test + spots_prop[spot]['y_sky_exp_center']
@@ -10767,7 +10769,7 @@ def ANTARESS_plot_functions(system_param,plot_dic,data_dic,gen_dic,coord_dic,the
                             #     plt.scatter(new_new_x_grid_test, new_new_y_grid_test, color='blue', alpha=0.5, s=8)
 
                             #     #Move coordinates in star frame in order to move them to the longitude and latitude of spot 
-                            #     st_x_grid_test, st_y_grid_test, st_z_grid_test = conv_inclinedStarFrame_to_StarFrame(new_new_x_grid_test, new_new_y_grid_test, new_z_grid_test, star_params['istar_rad'])
+                            #     st_x_grid_test, st_y_grid_test, st_z_grid_test = frameconv_InclinedStar_to_Star(new_new_x_grid_test, new_new_y_grid_test, new_z_grid_test, star_params['istar_rad'])
 
                             #     # plt.scatter(st_x_grid_test, st_y_grid_test, color='red', alpha=0.3, s=6)
 
@@ -10787,7 +10789,8 @@ def ANTARESS_plot_functions(system_param,plot_dic,data_dic,gen_dic,coord_dic,the
 
                             #     plt.scatter(new_new_x_grid_test[better_cond_in_sp], new_new_y_grid_test[better_cond_in_sp], color='red', alpha=0.7, s=6)
 
-
+                            #     #Put spot position
+                            #     plt.scatter(spots_prop[spot]['x_sky_exp_center'], spots_prop[spot]['y_sky_exp_center'], color='white', s=20)
 
 
                             #Testing - Spherical
@@ -11054,12 +11057,12 @@ def ANTARESS_plot_functions(system_param,plot_dic,data_dic,gen_dic,coord_dic,the
             plt.close()
             
             #Store image for GIF generation
-            if images_to_make_GIF is not None:images_to_make_GIF.append(imageio.imread(filename))
+            if images_to_make_GIF is not None:images_to_make_GIF.append(imageio.v2.imread(filename))
 
         ### End of loop on plotted timesteps    
 
         #Produce and store the GIF.
-        if images_to_make_GIF is not None:imageio.mimsave(path_loc+'System.gif', images_to_make_GIF,fps=plot_options[key_plot]['fps'])
+        if images_to_make_GIF is not None:imageio.mimsave(path_loc+'System.gif', images_to_make_GIF,duration=(1000 * 1/plot_options[key_plot]['fps']))
 
 
 

Method/ANTARESS_settings_mercier.py
@@ -191,13 +191,14 @@ def ANTARESS_settings(gen_dic,plot_dic,corr_spot_dic,data_dic,mock_dic,theo_dic,
     
     #Star name
 
-    gen_dic['star_name']='AUMic' # mercier
-    # gen_dic['star_name']='V1298tau'
+    # gen_dic['star_name']='AUMic' # mercier
+    gen_dic['star_name']='V1298tau'
 
     #Transiting planets
     if gen_dic['star_name']=='AUMic':
         gen_dic['transit_pl'] = {
-            'AUMicb':{'ESPRESSO' : ['mock_vis']}, 
+            'AUMicb':{'ESPRESSO' : ['mock_vis']},
+            # 'AUMicc':{'ESPRESSO' : ['mock_vis']}, 
             }
         gen_dic['kepl_pl'] = ['AUMicb']
     
@@ -441,7 +442,8 @@ def ANTARESS_settings(gen_dic,plot_dic,corr_spot_dic,data_dic,mock_dic,theo_dic,
     #Defining artificial visits
     if gen_dic['star_name'] == 'AUMic':
         mock_dic['visit_def']={
-            'ESPRESSO':{'mock_vis' :{'exp_range':2458330.39051+np.array([-0.2,0.2]),'nexp':50}}}
+            'ESPRESSO':{'mock_vis' :{'exp_range':2458330.39051+np.array([-0.15,0.15]),'nexp':50}}}
+            # 'ESPRESSO':{'mock_vis' :{'exp_range':2458330.39051+np.array([-0.15,0.15]),'nexp':250}}} #-- plotting purposes
             # 'ESPRESSO':{'mock_vis' :{'exp_range':2458330.39051+np.array([-2,2])/24.,'nexp':50}}}
                        
     if gen_dic['star_name'] == 'V1298tau':
@@ -486,9 +488,22 @@ def ANTARESS_settings(gen_dic,plot_dic,corr_spot_dic,data_dic,mock_dic,theo_dic,
                     
                      # #For the spot 'spot2' : 
                      'lat__ISESPRESSO_VSmock_vis_SPspot2'     : -20,
-                     'Tcenter__ISESPRESSO_VSmock_vis_SPspot2' : 2458330.39051 -0.2,
+                     'Tcenter__ISESPRESSO_VSmock_vis_SPspot2' : 2458330.39051 - 0.2,
                      'ang__ISESPRESSO_VSmock_vis_SPspot2'     : 25,
                      'atten__ISESPRESSO_VSmock_vis_SPspot2'    : 0.1,
+
+                     # For the spot 'spot1' : -- plotting purposes
+                     # 'lat__ISESPRESSO_VSmock_vis_SPspot1'     : 35,
+                     # 'Tcenter__ISESPRESSO_VSmock_vis_SPspot1' : 2458330.39051 + 0.3,
+                     # 'ang__ISESPRESSO_VSmock_vis_SPspot1'     : 15,
+                     # 'atten__ISESPRESSO_VSmock_vis_SPspot1'    : 0.95,
+                     
+
+                     # #For the spot 'spot2' :  -- plotting purposes
+                     # 'lat__ISESPRESSO_VSmock_vis_SPspot2'     : -15,
+                     # 'Tcenter__ISESPRESSO_VSmock_vis_SPspot2' : 2458330.39051 - 0.4,
+                     # 'ang__ISESPRESSO_VSmock_vis_SPspot2'     : 25,
+                     # 'atten__ISESPRESSO_VSmock_vis_SPspot2'    : 0.85,
                          }
                     }
                 }
@@ -904,7 +919,8 @@ def ANTARESS_settings(gen_dic,plot_dic,corr_spot_dic,data_dic,mock_dic,theo_dic,
 
     #Star discretization      
     if gen_dic['star_name']=='AUMic':
-        theo_dic['nsub_Dstar']=101.
+        theo_dic['nsub_Dstar']=201.
+        # theo_dic['nsub_Dstar']=301. #-- for plotting purposes
     
     if gen_dic['star_name']=='V1298tau':
         theo_dic['nsub_Dstar']=201.
@@ -916,10 +932,10 @@ def ANTARESS_settings(gen_dic,plot_dic,corr_spot_dic,data_dic,mock_dic,theo_dic,
 
     #Planet discretization
     if gen_dic['star_name']=='AUMic':
-        theo_dic['nsub_Dpl']= {'AUMicb':101.}                
+        theo_dic['nsub_Dpl']= {'AUMicb':101.}#, 'AUMicc':101.}                
 
     if gen_dic['star_name']=='V1298tau':
-        theo_dic['nsub_Dpl']={'V1298tau_b':21.}
+        theo_dic['nsub_Dpl']={'V1298tau_b':101.}
 
     #Spot discretization
     if gen_dic['star_name']=='AUMic':
@@ -929,7 +945,7 @@ def ANTARESS_settings(gen_dic,plot_dic,corr_spot_dic,data_dic,mock_dic,theo_dic,
         theo_dic['nsub_Dspot']={'spot1':50., 'spot2':50.}
     #Exposure discretization
     if gen_dic['star_name']=='AUMic':
-        theo_dic['n_oversamp']={'AUMicb':5.}
+        theo_dic['n_oversamp']={'AUMicb':5.}#, 'AUMicc': 5.}
         theo_dic['n_oversamp_spot']={'spot1':5., 'spot2':5.}
     
     if gen_dic['star_name']=='V1298tau':
@@ -2724,7 +2740,7 @@ def ANTARESS_settings(gen_dic,plot_dic,corr_spot_dic,data_dic,mock_dic,theo_dic,
         data_dic['DI']['system_prop']={
                 'achrom':{
                     'AUMicb' : [0.0512], #Gilbert et al. 2022
-                    #'AUMicc' : [0.001156], #Gilbert et al. 2022
+                    # 'AUMicc' : [0.1], #Gilbert et al. 2022
                     'LD' : ['quadratic'],
                     'LD_u1' : [0.35],
                     'LD_u2' : [0.16],
@@ -2744,11 +2760,12 @@ def ANTARESS_settings(gen_dic,plot_dic,corr_spot_dic,data_dic,mock_dic,theo_dic,
     if gen_dic['star_name']=='AUMic':
         data_dic['DI']['spots_prop']={
                 'achrom':{
-                    'spot1' : [15 * np.pi/180],
-                    'spot2' : [25 * np.pi/180],
+                    'spot1' : [mock_dic['spots_prop']['ESPRESSO']['mock_vis']['ang__ISESPRESSO_VSmock_vis_SPspot1'] * np.pi/180],
+                    'spot2' : [mock_dic['spots_prop']['ESPRESSO']['mock_vis']['ang__ISESPRESSO_VSmock_vis_SPspot2'] * np.pi/180],
                     'LD' : ['quadratic'],
                     'LD_u1' : [0.3],
                     'LD_u2' : [0.16],
+                    'veq_spots' : [7.8],
 
                 },
                 }
@@ -2756,10 +2773,11 @@ def ANTARESS_settings(gen_dic,plot_dic,corr_spot_dic,data_dic,mock_dic,theo_dic,
     if gen_dic['star_name']=='V1298tau':
         data_dic['DI']['spots_prop']={
                 'achrom':{
-                    'spot1' : [20 * np.pi/180],
-                    'spot2' : [25 * np.pi/180],
-                    'LD' : ['quadratic'],
+                    'spot1' : [mock_dic['spots_prop']['ESPRESSO']['mock_vis']['ang__ISESPRESSO_VSmock_vis_SPspot1'] * np.pi/180],
+                    'spot2' : [mock_dic['spots_prop']['ESPRESSO']['mock_vis']['ang__ISESPRESSO_VSmock_vis_SPspot2'] * np.pi/180],
+                    'LD' : ['linear'],
                     'LD_u1' : [0.41],
+                    'veq_spots' : [23.59],
 
                 },
                 }