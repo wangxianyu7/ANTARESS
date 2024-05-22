49bcf87690f6d1c61b7793b5d82b88818ee7f44b
Vincent Bourrier <vincent.bourrier@unige.ch>
2024-05-22 14:28:06 +0000

    Replace setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from setuptools import Extension, setup
 setup(
   ext_modules=[
     Extension(
+     'antaress.Gauss_star_grid',
       sources=['src/antaress/ANTARESS_analysis/C_grid/Gauss_star_grid.c'],
       language='c',
     )
